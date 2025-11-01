5b6e07b9954be955430fa92c4bfd09e38af60839
Juroxa <juuso.sav@gmail.com>
2025-11-01 12:50:03 +0200

    First iteration of Expense Tracker

---
+++ ExpenseTracker/Helpers.cs
@@ -0,0 +1,229 @@
+﻿using System;
+using System.Text.Json;
+
+class Helpers
+{
+    public static void ClearBelowMenu(int menuLines)
+    {
+        // Set cursor position below the menu
+        Console.SetCursorPosition(0, menuLines);
+
+        // Clear lines below the menu
+        for (int i = menuLines; i < Console.WindowHeight; i++)
+        {
+            Console.SetCursorPosition(0, i);
+            Console.Write(new string(' ', Console.WindowWidth));
+        }
+
+        // Reset cursor to the menu's end
+        Console.SetCursorPosition(0, menuLines);
+    }
+    public static void Display(Dictionary<string, double> items, string dictName)
+    {
+        Console.WriteLine($"\n{dictName.ToUpper()}");
+        Console.WriteLine("-----------");
+
+        int i = 1;
+        foreach (var item in items)
+        {
+            //Displays the expense list of the selected dictionary
+            Console.WriteLine($"{i}. {item.Key} - {item.Value}€");
+            i++;
+
+        }
+    }
+
+    public static string DisplayAll(
+        Dictionary<string, double> items1,
+        Dictionary<string, double> items2,
+        Dictionary<string, double> items3,
+        Dictionary<string, double> items4,
+        string dictName1,
+        string dictName2,
+        string dictName3,
+        string dictName4)
+    {
+        //Enables saving to a text file
+        var sb = new System.Text.StringBuilder();
+
+        void appendDict(Dictionary<string, double> items, string dictName)
+        {
+            sb.AppendLine($"\n{dictName.ToUpper()}");
+            sb.AppendLine("-----------");
+
+            int i = 1;
+            foreach (var item in items)
+            {
+                //Displays the expense list of the selected dictionary
+                sb.AppendLine($"{i}. {item.Key} - {item.Value}€");
+                i++;
+            }
+            sb.AppendLine("-----------");
+            sb.AppendLine($" Total: {items.Values.Sum()}€");
+        }
+        appendDict(items1, dictName1);
+        appendDict(items2, dictName2);
+        appendDict(items3, dictName3);
+        appendDict(items4, dictName4);
+
+        return sb.ToString();
+    }
+
+    public static void Add(Dictionary<string, double> items, string dictName)
+    {
+
+        Console.WriteLine($"\nPlease specify an item to add to: {dictName}");
+        Console.Write("Item: ");
+
+        string? expenseChoice = Console.ReadLine();
+
+        if (!string.IsNullOrEmpty(expenseChoice))
+        {
+            expenseChoice = char.ToUpper(expenseChoice[0]) + expenseChoice[1..].ToLower();
+            Console.WriteLine($"\n{expenseChoice} added to list: {dictName}");
+        }
+        else
+        {
+            Console.WriteLine("\nYou tried to enter a empty list item.\n");
+            Console.WriteLine("Press any key to return to menu.");
+            Console.ReadKey();
+            return;
+        }
+
+        Console.WriteLine("\nPlease specify the price of the expense (format: xx,xx) ");
+        Console.Write("Price: ");
+        string? input = Console.ReadLine();
+        input = input?.Replace(".", ",");
+
+        try
+        {
+            if (double.TryParse(input, out double number))
+            {
+                number = Math.Round(number, 2);
+                items.Add(expenseChoice, number);
+            }
+            else
+            {
+                Console.WriteLine("\nPlease enter a valid number");
+                Console.WriteLine("Press any key to return to menu.");
+                Console.ReadKey();
+                return;
+            }
+        }
+        catch (ArgumentException)
+        {
+            Console.WriteLine($"\nThe item \"{expenseChoice}\" already exits in that list. Please remove the item if you want to re-enter it.");
+            Console.WriteLine("Press any key to return to menu.");
+            Console.ReadKey();
+            return;
+        }
+
+        Console.WriteLine($"\nItem added to: {dictName}!\n");
+        Console.WriteLine("Press any key to return to menu.");
+        Console.ReadKey();
+    }
+
+    public static void Remove(Dictionary<string, double> items, string dictName)
+    {
+        Console.WriteLine($"\nPlease specify an item to remove from: {dictName}");
+        Console.WriteLine("");
+        Helpers.Display(items, dictName);
+        Console.Write("\nItem: ");
+
+        string? RemoveChoice = Console.ReadLine();
+
+        if (!string.IsNullOrEmpty(RemoveChoice) && int.TryParse(RemoveChoice, out int number))
+        {
+            if (number >= 1 && number <= items.Count)
+            {
+                var keyToRemove = items.Keys.ElementAt(number - 1);
+                items.Remove(keyToRemove);
+                Console.WriteLine($"\nThe item \"{keyToRemove}\" was removed from list: {dictName}\n");
+            }
+            else
+            {
+                Console.WriteLine("Please enter a valid list number.");
+            }
+            Console.WriteLine("Press any key to return to menu.");
+            Console.ReadKey();
+
+        }
+        else
+        {
+            Console.WriteLine("\nItem not found, please enter a valid list number.\n");
+            Console.WriteLine("Press any key to return to menu.");
+            Console.ReadKey();
+            return;
+        }
+
+    }
+
+    public static void ListSum(Dictionary<string, double> items)
+    {
+        // Adds all the item values together
+        double? totalSum = items.Values.Sum();
+        Console.WriteLine("-----------");
+        Console.WriteLine($"\nTotal: {totalSum}€\n");
+        Console.WriteLine("Press any key to return to menu.");
+        Console.ReadKey();
+    }
+
+    public static Dictionary<string, double> LoadExpenses(string filePath)
+    {
+        //Loads expenses from json file (if found)
+        if (!File.Exists(filePath)) return [];
+        string json = File.ReadAllText(filePath);
+        try
+        {
+            return JsonSerializer.Deserialize<Dictionary<string, double>>(json) ?? [];
+        }
+        catch
+        {
+            Console.WriteLine($"Error loading {filePath}. Creating a new empty file.");
+            return [];
+        }
+    }
+
+    public static void SaveExpenses(Dictionary<string, double> data, string path)
+    {
+        try
+        {
+            File.WriteAllText(path, JsonSerializer.Serialize(data));
+        }
+        catch (IOException ex)
+        {
+            Console.WriteLine($"Error saving to {path}: {ex.Message}");
+        }
+    }
+
+    public static void ClearAllLists(
+        Dictionary<string, double> items1,
+        Dictionary<string, double> items2,
+        Dictionary<string, double> items3,
+        Dictionary<string, double> items4)
+    {
+        items1.Clear();
+        items2.Clear();
+        items3.Clear();
+        items4.Clear();
+    }
+
+    //Lists all values added together
+    public static void ListAllSum(Dictionary<string, double> items1,
+        Dictionary<string, double> items2,
+        Dictionary<string, double> items3,
+        Dictionary<string, double> items4)
+    {
+        double value1 = items1.Values.Sum();
+        double value2 = items2.Values.Sum();
+        double value3 = items3.Values.Sum();
+        double value4 = items4.Values.Sum();
+
+        double allValuesAdded = value1 + value2 + value3 + value4;
+
+        Console.WriteLine("\n-------------------");
+        Console.WriteLine($"Total of all expenses: {allValuesAdded}€");
+        Console.WriteLine("\nPress any key to return to menu.");
+        Console.ReadKey();
+    }
+}
---
+++ ExpenseTracker/Program.cs
@@ -0,0 +1,319 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.Json;
+using System.Threading.Tasks;
+using System.Globalization;
+
+
+namespace ExpenseTracker
+{
+
+    class Program
+    {
+        static void Main()
+        {
+
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+            //Loads the current saved expenses from a .json file
+            var groceries = Helpers.LoadExpenses("groceries.json");
+            var clothing = Helpers.LoadExpenses("clothing.json");
+            var housing = Helpers.LoadExpenses("housing.json");
+            var other = Helpers.LoadExpenses("other.json");
+
+            //Program start:
+            while (true)
+            {
+                Console.Clear();
+
+                Console.WriteLine("EXPENSE TRACKER");
+                Console.WriteLine("---------------");
+
+                //Start menu
+                Console.WriteLine("\nPlease choose 1-5 and press Enter.");
+                Console.WriteLine("\n1. Add expense");
+                Console.WriteLine("2. Remove expense");
+                Console.WriteLine("3. Display expenses");
+                Console.WriteLine("4. Clear expense list");
+                Console.WriteLine("5. Export expenses");
+                Console.WriteLine("6. Exit application\n");
+
+                Console.Write("Your choice: ");
+                string? initialChoice = Console.ReadLine();
+
+                //First choose an operation, after that, choosing a dictionary is prompted
+                switch (initialChoice)
+                {
+                    // Add item to a dictionary
+                    case "1":
+                        Console.WriteLine("\nPlease choose an expense to add");
+                        Console.WriteLine("(Write the number of your choice, and press Enter)");
+                        Console.WriteLine("\n1. Groceries\n2. Clothing\n3. Housing\n4. Other\n");
+                        Console.Write("Your choice: ");
+
+                        string? addExpenseChoice = Console.ReadLine();
+
+
+                        if (addExpenseChoice == "1" && addExpenseChoice != null)
+                        {
+                            Helpers.Add(groceries, "Groceries");
+                            Helpers.SaveExpenses(groceries, "groceries.json");
+                        }
+
+                        else if (addExpenseChoice == "2" && addExpenseChoice != null)
+                        {
+                            Helpers.Add(clothing, "Clothing");
+                            Helpers.SaveExpenses(clothing, "clothing.json");
+                        }
+
+                        else if (addExpenseChoice == "3" && addExpenseChoice != null)
+                        {
+                            Helpers.Add(housing, "Housing");
+                            Helpers.SaveExpenses(housing, "housing.json");
+                        }
+
+                        else if (addExpenseChoice == "4" && addExpenseChoice != null)
+                        {
+                            Helpers.Add(other, "Other");
+                            Helpers.SaveExpenses(other, "other.json");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Please enter a valid choice.");
+                            Console.WriteLine("Press any key to return to menu.");
+                            Console.ReadKey();
+                        }
+                        break;
+
+                    //Remove item from a dictionary.
+                    case "2":
+                        Console.WriteLine("\nPlease choose an expense to remove");
+                        Console.WriteLine("(Write the number of your choice, and press Enter)");
+                        Console.WriteLine("\n1. Groceries\n2. Clothing\n3. Housing\n4. Other\n");
+                        Console.Write("Your choice: ");
+
+                        string? removeExpenseChoice = Console.ReadLine();
+
+                        if (removeExpenseChoice == "1" && removeExpenseChoice != null && groceries.Count > 0)
+                        {
+                            Helpers.Remove(groceries, "Groceries");
+                            Helpers.SaveExpenses(groceries, "groceries.json");
+                        }
+                        else if (removeExpenseChoice == "2" && removeExpenseChoice != null && clothing.Count > 0)
+                        {
+                            Helpers.Remove(clothing, "Clothing");
+                            Helpers.SaveExpenses(clothing, "clothing.json");
+                        }
+                        else if (removeExpenseChoice == "3" && removeExpenseChoice != null && housing.Count > 0)
+                        {
+                            Helpers.Remove(housing, "Housing");
+                            Helpers.SaveExpenses(housing, "housing.json");
+                        }
+                        else if (removeExpenseChoice == "4" && removeExpenseChoice != null && other.Count > 0)
+                        {
+                            Helpers.Remove(other, "Other");
+                            Helpers.SaveExpenses(other, "other.json");
+                        }
+
+                        else
+                        {
+                            Console.WriteLine("\nThe selected list was empty, or your choice was not valid.");
+                            Console.WriteLine("Press any key to return to menu.");
+                            Console.ReadKey();
+
+                        }
+                        break;
+
+                    //Display dictionaries
+                    case "3":
+                        Console.WriteLine("\nPlease choose an expense list to display");
+                        Console.WriteLine("(Write the number of your choice, and press Enter)");
+                        Console.WriteLine("\n1. Groceries\n2. Clothing\n3. Housing\n4. Other\n5. All expense lists");
+                        Console.Write("\nYour choice: ");
+
+                        string? listDisplayChoice = Console.ReadLine();
+
+                        if (listDisplayChoice == "1")
+                        {
+                            Helpers.Display(groceries, "Groceries");
+                            Console.WriteLine("");
+
+                            Helpers.ListSum(groceries);
+                        }
+
+                        else if (listDisplayChoice == "2")
+                        {
+
+                            Helpers.Display(clothing, "Clothing");
+                            Console.WriteLine("");
+
+                            Helpers.ListSum(clothing);
+                        }
+
+                        else if (listDisplayChoice == "3")
+                        {
+                            Helpers.Display(housing, "Housing");
+                            Console.WriteLine("");
+
+                            Helpers.ListSum(housing);
+
+                        }
+                        else if (listDisplayChoice == "4")
+                        {
+                            Helpers.Display(other, "Other");
+                            Console.WriteLine("");
+
+                            Helpers.ListSum(other);
+
+                        }
+                        else if (listDisplayChoice == "5")
+                        {
+                            Console.WriteLine(Helpers.DisplayAll(groceries, clothing, housing, other, "Groceries", "Clothing", "Housing", "Other"));
+                            Console.WriteLine("");
+                            Helpers.ListAllSum(groceries, clothing, housing, other);
+                        }
+                        else
+                        {
+                            Console.WriteLine("Please enter a valid choice.");
+                            Console.WriteLine("Press any key to return to menu.");
+                            Console.ReadKey();
+                        }
+                        break;
+
+                    //Clears a dictionary, or clears all dictionaries
+                    case "4":
+                        Console.WriteLine("\nPlease choose an expense list to clear");
+                        Console.WriteLine("(Write your choice, and press Enter)");
+                        Console.WriteLine("\n1. Groceries\n2. Clothing\n3. Housing\n4. Other\n5. All expense lists");
+                        Console.Write("\nYour choice: ");
+
+                        string? clearChoice = Console.ReadLine();
+
+                        if (clearChoice == "1")
+                        {
+                            groceries.Clear();
+                            Console.WriteLine("");
+                            Helpers.SaveExpenses(groceries, "groceries.json");
+
+                            Console.WriteLine("Groceries list cleared.");
+                            break;
+                        }
+
+                        else if (clearChoice == "2")
+                        {
+
+                            clothing.Clear();
+                            Console.WriteLine("");
+                            Helpers.SaveExpenses(clothing, "clothing.json");
+
+                            Console.WriteLine("Clothing list cleared.");
+                            break;
+                        }
+
+                        else if (clearChoice == "3")
+                        {
+                            housing.Clear();
+                            Console.WriteLine("");
+                            Helpers.SaveExpenses(housing, "housing.json");
+
+                            Console.WriteLine("Housing list cleared.");
+                            break;
+
+                        }
+                        else if (clearChoice == "4")
+                        {
+                            other.Clear();
+                            Console.WriteLine("");
+                            Helpers.SaveExpenses(other, "other.json");
+
+                            Console.WriteLine("Other items list cleared.");
+                            break;
+
+                        }
+                        else if (clearChoice == "5")
+                        {
+                            Console.WriteLine("\nAre you sure you want to clear all expense lists (y/n)?");
+                            Console.Write("Your choice: ");
+                            string? verifyChoice = Console.ReadLine();
+
+                            if (verifyChoice == "y")
+                            {
+                                Helpers.ClearAllLists(groceries, clothing, housing, other);
+                                Helpers.SaveExpenses(groceries, "groceries.json");
+                                Helpers.SaveExpenses(clothing, "clothing.json");
+                                Helpers.SaveExpenses(housing, "housing.json");
+                                Helpers.SaveExpenses(other, "other.json");
+                                Console.WriteLine("");
+                            }
+                            else if (verifyChoice == "n")
+                            {
+                                Console.WriteLine("\nOperation aborted.");
+                                Console.WriteLine("Press any key to return to menu.");
+                                Console.ReadKey();
+                                break;
+                            }
+                            else
+                            {
+                                Console.WriteLine("\nNeither 'y' or 'n' was chosen. Aborting operation.");
+                                Console.WriteLine("Press any key to return to menu.");
+                                Console.ReadKey();
+                                break;
+                            }
+                        }
+
+                        else
+                        {
+                            Console.WriteLine("\nPlease enter a valid choice.");
+                            Console.WriteLine("Press any key to return to menu.");
+                            Console.ReadKey();
+                            break;
+                        }
+
+                        Console.WriteLine("All expenses cleared.");
+                        Console.WriteLine("Press any key to return to menu.");
+                        Console.ReadKey();
+                        break;
+
+                    //Saves expenses to a text file
+                    case "5":
+
+                        string report = Helpers.DisplayAll(groceries, clothing, housing, other,
+                                            "Groceries", "Clothing", "Housing", "Other");
+
+                        string fileName = "expenses.txt";
+
+                        File.WriteAllText(fileName, report);
+
+                        Console.WriteLine($"\nAll expenses saved to {fileName}.");
+                        Console.WriteLine("Press any key to return to menu.");
+                        Console.ReadKey();
+                        break;
+
+                    //Basic application exit operation
+                    case "6":
+                        Console.Write("\nExiting application.");
+                        Thread.Sleep(500);
+                        Console.Write(".");
+                        Thread.Sleep(500);
+                        Console.Write(".");
+                        Thread.Sleep(500);
+                        Environment.Exit(0);
+                        break;
+
+                    //This runs if the start menu choice is not valid
+                    default:
+                        Console.WriteLine("\nPlease enter a valid choice.");
+                        Console.WriteLine("Press any key to return to menu.");
+                        Console.ReadKey();
+                        break;
+                }
+                //This clears the console, so that only the start menu is visible after an operation
+                Helpers.ClearBelowMenu(11);
+            }
+
+        }
+    }
+}
+
